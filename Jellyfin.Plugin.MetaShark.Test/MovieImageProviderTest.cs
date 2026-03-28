05732ec658da125b86bfbcd17ae704099eaeb56e
qimaoww <qimaoaa@hotmail.com>
2026-03-29 00:55:49 +0800

    电影图片在 Douban 被封时回退 TMDb

--- Jellyfin.Plugin.MetaShark.Test/MovieImageProviderTest.cs
+++ Jellyfin.Plugin.MetaShark.Test/MovieImageProviderTest.cs
@@ -11,6 +11,8 @@ using Moq;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -103,6 +105,44 @@ namespace Jellyfin.Plugin.MetaShark.Test
 
                 var str = result.ToJson();
                 Console.WriteLine(result.ToJson());
+            }).GetAwaiter().GetResult();
+        }
+
+        [TestMethod]
+        public void TestGetImagesFallsBackToTmdbWhenDoubanBlocked()
+        {
+            var info = new MediaBrowser.Controller.Entities.Movies.Movie()
+            {
+                Name = "秒速5厘米",
+                PreferredMetadataLanguage = "zh",
+                ProviderIds = new Dictionary<string, string>
+                {
+                    { BaseProvider.DoubanProviderId, "2043546" },
+                    { MetadataProvider.Tmdb.ToString(), "38142" },
+                },
+            };
+            var httpClientFactory = new DefaultHttpClientFactory();
+            var libraryManagerStub = new Mock<ILibraryManager>();
+            var httpContextAccessorStub = new Mock<IHttpContextAccessor>();
+            var doubanApi = DoubanApiTestHelper.CreateBlockedDoubanApi(loggerFactory);
+            var tmdbApi = new TmdbApi(loggerFactory);
+            var omdbApi = new OmdbApi(loggerFactory);
+            var imdbApi = new ImdbApi(loggerFactory);
+
+            Task.Run(async () =>
+            {
+                try
+                {
+                    var provider = new MovieImageProvider(httpClientFactory, loggerFactory, libraryManagerStub.Object, httpContextAccessorStub.Object, doubanApi, tmdbApi, omdbApi, imdbApi);
+                    var images = (await provider.GetImages(info, CancellationToken.None)).ToList();
+                    Assert.IsTrue(images.Any(), "Douban blocked 后应继续回退 TMDb 并返回图片。");
+                    Assert.IsTrue(images.Any(image => image.Url?.Contains("tmdb", StringComparison.OrdinalIgnoreCase) == true), "应返回 TMDb 图片 URL。");
+                }
+                catch (HttpRequestException ex) when (ex.StatusCode == HttpStatusCode.TooManyRequests
+                    || ex.Message.Contains("429", StringComparison.Ordinal))
+                {
+                    Assert.Inconclusive("TMDb rate limited (429)." + ex.Message);
+                }
             }).GetAwaiter().GetResult();
         }
     }
